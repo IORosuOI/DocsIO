99ab4688b94b01c2a77a0eed454f1583901a15df
IORosuOI <rosuioanluca@gmail.com>
2026-05-22 04:38:56 +0300

    feat: add service interfaces

---
+++ backend/src/main/java/com/docsio/backend/service/UserService.java
@@ -0,0 +1,10 @@
+package com.docsio.backend.service;
+
+import com.docsio.backend.domain.User;
+import java.util.List;
+
+public interface UserService {
+    User save(User user);
+    List<User> findAll();
+    void delete(Long id);
+}
